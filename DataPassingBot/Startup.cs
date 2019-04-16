646b5160d3d2643f62880fabbf77a4fe1f45065a
Martin Kearn <martin.kearn@microsoft.com>
2019-04-16 08:13:42 +0100

    Initial StarterBot project template

---
+++ DataPassingBot/Startup.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using DataPassingBot.Bots;
+using DataPassingBot.Dialogs.Root;
+using DataPassingBot.Helpers;
+using DataPassingBot.Interfaces;
+using DataPassingBot.Services;
+
+namespace DataPassingBot
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IBotServices, BotServices>();
+
+            services.AddSingleton<IBotFrameworkHttpAdapter, BotFrameworkHttpAdapter>();
+
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            services.AddSingleton<UserState>();
+
+            services.AddSingleton<ConversationState>();
+
+            services.AddSingleton<RootDialog>();
+
+            services.AddTransient<IBot, MainBot<RootDialog>>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseMvc();
+        }
+    }
+}
